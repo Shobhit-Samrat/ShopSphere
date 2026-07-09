ab1390cdcfaac05149b98ba0aba58e70e400644c
Shobhit-Samrat <shubraj0408@gmail.com>
2026-07-10 02:50:19 +0530

    Update API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://shopsphere-api-4lff.onrender.com/api",
 
     headers: {
         "Content-Type": "application/json",
